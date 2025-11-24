bc4a892e101ae84add7f07d67bf17b412465a526
Raphael Abreu <raphaelrat@hotmail.com>
2025-11-23 21:10:19 -0300

    chore: bootstrap very_good_coffee with localization

---
+++ lib/app/view/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:very_good_coffee/l10n/l10n.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const _TinderCoffee(),
+    );
+  }
+}
+
+class _TinderCoffee extends StatelessWidget {
+  const _TinderCoffee();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
+      body: const Center(child: Icon(Icons.construction)),
+    );
+  }
+}
